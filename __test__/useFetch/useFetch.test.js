6bc4972b52ca50a78dc6e157a5cd5a084c463aaa
Louai99k <louaikelani99k@gmail.com>
2022-08-21 21:04:29 +0300

    adding use-fetch tests

---
+++ __test__/useFetch/useFetch.test.js
@@ -0,0 +1,44 @@
+import { describe, it, expect } from "vitest";
+import useFetch from "hooks/use-fetch";
+import { renderHook, waitFor } from "@testing-library/react";
+import { initCache } from "hooks/use-fetch/helpers/cache";
+import * as types from "hooks/use-fetch/helpers/types"; // eslint-disable-line no-unused-vars
+import createFetchMock from "vitest-fetch-mock";
+import { vi } from "vitest";
+createFetchMock(vi);
+
+describe("useFetch Hook", () => {
+  it("to fetch data", async () => {
+    const { result } = renderHook(() =>
+      useFetch("http://localhost:1600/products")
+    );
+    const { loading } = result.current;
+    expect(loading).toEqual(true);
+    await waitFor(
+      () => {
+        const { data, error } = result.current;
+        expect(data).toBeTruthy();
+        expect(error).toBeNull();
+      },
+      { timeout: 1500, interval: 500 }
+    );
+  });
+  it("to cache data and have the cache props", async () => {
+    await waitFor(
+      () => {
+        const [get] = initCache();
+        /**@type {types.CacheRecord} */
+        const cachedData = get("http://localhost:1600/products");
+        expect(cachedData).toHaveProperty("data");
+        expect(cachedData).toHaveProperty("onMutate");
+        expect(cachedData).toHaveProperty("config");
+        expect(cachedData).toHaveProperty("error");
+        expect(cachedData).toHaveProperty("isValidating");
+        const { data, error } = cachedData;
+        expect(Array.isArray(data)).toBe(true); // for this api endpoint
+        expect(error).toBeNull();
+      },
+      { timeout: 1500, interval: 500 }
+    );
+  });
+});
