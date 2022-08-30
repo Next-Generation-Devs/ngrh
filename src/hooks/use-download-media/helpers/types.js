5bbc2bfee272aecfe26048589e09e7079886a9de
Louai99k <louaikelani99k@gmail.com>
2022-08-30 22:59:24 +0300

    minor fixes

--- src/hooks/use-download-media/helpers/types.js
+++ src/hooks/use-download-media/helpers/types.js
@@ -1,7 +1,7 @@
 /**
- * @typedef {Files}
+ * @typedef {Object} HookFiles
  * @prop {string} id - the id of the file.
- * @prop {} chunks - the downloaded chunks.
+ * @prop {Uint8Array} chunks - the downloaded chunks.
  * @prop {string} fileName - the name of the file.
  * @prop {number} loading - the downloading state.
  */
@@ -10,20 +10,21 @@
  * @typedef {Object} ReturnObject
  * @prop {Download} download - a function used to download sources to browser and from there will be ready to download locally.
  * @prop {GetLoadingById} getLoadingById - gets the download progress.
- * @prop {Array<Files>} files - the files array.
- * @prop {DonwloadToLocal} donwloadToLocal - a function used to download sources locally.
+ * @prop {Array<HookFiles>} files - the files array.
+ * @prop {DownloadToLocal} downloadToLocal - a function used to download sources locally.
  */
 
 /**
  * @callback useDownloadMedia
- * @return {ReturnObject} ```{ donwload, getLoadingById, files , VoidFunction }```
+ * @return {ReturnObject} ```{ donwload, getLoadingById, files , downloadToLocal }```
  */
 
 /**
  * @typedef {Object} downloadOptions
  * @prop {string} url - the url of the source.
- * @prop {string} fileName - a name to the downloaded file.
- * @prop {string} id - an id given to the downloaded file to get download progress.
+ * @prop {string} fileName - the name of the file (should include the extension).
+ * @prop {string} id - the id to identify the file (used in `getLoadingById` and `downloadToLocal`).
+ * @prop {boolean} withAutoDownload - a flag to determine if the file will be downloaded automatically after being downloaded to the borwser or not (_defaults to false_).
  */
 
 /**
@@ -33,7 +34,7 @@
  */
 
 /**
- * @callback DonwloadToLocal
+ * @callback DownloadToLocal
  * @param {string} id - the id of the downloaded file.
  * @return {void}
  */
--- src/hooks/use-download-media/use-download-media.js
+++ src/hooks/use-download-media/use-download-media.js
@@ -33,64 +33,12 @@ const reducer = (state, action) => {
 };
 
 /**
- * @type {types.useFetch}
+ * @type {types.useDownloadMedia}
  */
-function useDownloadMedia() {
+export const useDownloadMedia = () => {
   const [files, dispatch] = useReducer(reducer, []);
 
-  /**
-   * @type {types.download}
-   */
-  const donwload = async ({
-    url,
-    fileName = url.replace(/^.*[\\/]/g, ""),
-    id,
-  }) => {
-    try {
-      let response = await fetch(url);
-
-      const reader = response.body.getReader();
-      const contentLength = +response.headers.get("content-length");
-      let receivedLength = 0;
-      let chunks = [];
-      if (!files.find((el) => el.id === id)) {
-        dispatch({
-          type: "CREATE_LOADING",
-          id: id,
-          fileName,
-          chunks,
-          loading: 0,
-        });
-      }
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        const { done, value } = await reader.read();
-
-        if (done) {
-          break;
-        }
-        chunks.push(value);
-        receivedLength += value.length;
-
-        dispatch({
-          type: "UPDATE_LOADING",
-          id,
-          loading: Math.floor((receivedLength * 100) / contentLength),
-        });
-      }
-      dispatch({
-        type: "UPDATE_CHUNKS",
-        id,
-        chunks,
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  /**
-   * @type {types.donwloadToLocal}
-   */
-  const donwloadToLocal = (id) => {
+  const downloadToLocal = (id) => {
     const file = files.find((el) => el.id === id);
     if (file) {
       let blob = new Blob(file.chunks);
@@ -101,9 +49,7 @@ function useDownloadMedia() {
       a.remove();
     }
   };
-  /**
-   * @type {types.getLoadingById}
-   */
+
   const getLoadingById = (id) => {
     const temp = files.find((el) => el.id === id);
     if (temp) {
@@ -112,6 +58,59 @@ function useDownloadMedia() {
     return -1;
   };
 
-  return { donwload, getLoadingById, files, donwloadToLocal };
-}
-export default useDownloadMedia;
+  const download = async ({
+    url,
+    fileName = url.replace(/^.*[\\/]/g, ""),
+    id,
+    withAutoDownload = false,
+  }) => {
+    let response;
+    try {
+      response = await fetch(url);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+    const reader = response.body.getReader();
+    const contentLength = +response.headers.get("content-length");
+    let receivedLength = 0;
+    let chunks = [];
+    if (!files.find((el) => el.id === id)) {
+      dispatch({
+        type: "CREATE_LOADING",
+        id: id,
+        fileName,
+        chunks,
+        loading: 0,
+      });
+    }
+    const condition = true;
+    while (condition) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      chunks.push(value);
+      receivedLength += value.length;
+      dispatch({
+        type: "UPDATE_LOADING",
+        id,
+        loading: Math.floor((receivedLength * 100) / contentLength),
+      });
+    }
+    dispatch({
+      type: "UPDATE_CHUNKS",
+      id,
+      chunks,
+    });
+    if (withAutoDownload) {
+      let blob = new Blob(chunks);
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(blob);
+      a.download = fileName;
+      a.click();
+      a.remove();
+    }
+  };
+
+  return { download, getLoadingById, files, downloadToLocal };
+};
