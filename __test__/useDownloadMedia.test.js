839dd5a9ed0692c2919b83a32a4308a2806e55c2
Louai99k <louaikelani99k@gmail.com>
2022-08-30 22:59:28 +0300

    adding tests

--- __test__/useDownloadMedia.test.js
+++ __test__/useDownloadMedia.test.js
@@ -1,26 +1,61 @@
-import { renderHook, waitFor, act } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import useDownloadMedia from "hooks/use-download-media";
-import { describe, it, expect } from "vitest";
-import createFetchMock from "vitest-fetch-mock";
-import { vi } from "vitest";
-createFetchMock(vi);
+import { describe, it, beforeAll, expect } from "vitest";
+
+const sleep = async (amount) => {
+  await new Promise((res) =>
+    setTimeout(res, Math.floor(Math.random() * amount))
+  );
+};
 
 describe("useDownloadMedia", () => {
+  beforeAll(() => {
+    // eslint-disable-next-line no-undef
+    global.fetch = () =>
+      new Promise((resolve) => {
+        resolve({
+          headers: {
+            get() {
+              return 100;
+            },
+          },
+          body: {
+            getReader() {
+              let i = 0;
+              return {
+                async read() {
+                  if (i < 100) {
+                    i++;
+                    await sleep(5);
+                    return {
+                      value: { length: 1 },
+                      done: false,
+                    };
+                  }
+                  return { value: undefined, done: true };
+                },
+              };
+            },
+          },
+        });
+      });
+  });
   it("should set download loading to 100", async () => {
     const { result } = renderHook(() => useDownloadMedia());
-    const { donwload } = result.current;
-    await act(async () => {
-      await donwload({
+    const { download } = result.current;
+    act(() => {
+      download({
         url: "https://ngrh-test-server.onrender.com/images/test.pdf",
-        id: "file1",
-        fileName: "pdffile",
+        id: "1",
+        fileName: "testFile",
       });
-      console.log(result.current.files);
     });
-    const { getLoadingById } = result.current;
-    expect(getLoadingById("file1")).toEqual(100);
-    // expect(files.length).toBe(1);
-
-    // expect(donwloadToLocal("file1")).toBeCalled();
+    await waitFor(
+      () => {
+        const downloaded = result.current.getLoadingById("1");
+        expect(downloaded).toEqual(100);
+      },
+      { timeout: 3000 }
+    );
   });
 });
