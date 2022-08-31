ed5522c1e52dbfa799565aec2275f5299f63893b
Louai99k <louaikelani99k@gmail.com>
2022-09-01 00:32:25 +0300

    adding core files

---
+++ src/hooks/use-search/helpers/config.js
@@ -0,0 +1,13 @@
+import * as types from "./types"; // eslint-disable-line no-unused-vars
+
+/**
+ * @type {types.GetDefaultOptions}
+ */
+
+export const getDefaultOptions = () => {
+  return {
+    parsePaths: false,
+    strictFilter: false,
+    searchType: "filter",
+  };
+};
